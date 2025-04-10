f79530638c8706aef4bc0339f38e1837740914bb
imjoyhalder <joyhalder00113355@gamil.com>
2025-04-10 14:15:49 +0600

    tailwind css, google font and README file setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
