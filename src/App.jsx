0ea42c608b677c09865f11c4142847911a40be52
imjoyhalder <joyhalder00113355@gamil.com>
2025-04-10 14:43:32 +0600

    Navbar section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <>
-      
+      <Navbar></Navbar>
     </>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import logo from '../../assets/image/logo.png';
+import coin from '../../assets/image/icons8-coin.gif';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1 className='text-sm'>This is form Navbar components</h1>
+        <div className='w-11/12 mx-auto flex justify-between m-10 items-center static'>
+            <div>
+                <img className='w-20' src={logo} alt="" />
+            </div>
+            <div className='flex gap-12 items-center'>  
+                <a className=' text-gray-600' href="">Home</a>
+                <a className=' text-gray-600' href="">Fixture</a>
+                <a className=' text-gray-600' href="">Teams</a>
+                <a className=' text-gray-600' href="">Schedules</a>
+                <div className='border-2 font-bold bg-yellow-50 rounded-xl p-2 flex gap-1'>
+                    <p>0</p>
+                    <p>Coin</p>
+                    <img className='w-6' src={coin} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
